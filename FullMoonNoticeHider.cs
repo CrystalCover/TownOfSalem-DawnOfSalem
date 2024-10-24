77bf620484248d97132beb3264d47c370bca6c3f
Eca <65168018+CrystalCover@users.noreply.github.com>
2024-10-24 17:19:20 +0300

    Update FullMoonNoticeHider.cs

--- FullMoonNoticeHider.cs
+++ FullMoonNoticeHider.cs
@@ -1,11 +1,11 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using System.Collections.Generic;
 using System.Linq;
 using static CustomGameSetup.Role;
 
 namespace Eca.DawnOfSalem
 {
-    [HarmonyPatch(typeof(GameSceneChatListener))]
+    [HarmonyPatch(typeof(GameSceneChatListener), "HandleOnFullMoonNight")]
     internal class FullMoonNoticeHider
     {
         private static IGameService GameService => GlobalServiceLocator.GameService;
@@ -18,8 +18,6 @@ namespace Eca.DawnOfSalem
 
         private static Dictionary<int, Role> Roles => GameRules.Roles;
 
-        [HarmonyPatch("HandleOnFullMoonNight")]
-        [HarmonyPrefix]
-        private static bool HandleOnFullMoonNight() => new List<CustomGameSetup.Role>(new[] { WEREWOLF, RANDOM_NEUTRAL, NEUTRAL_KILLING, ANY, JUGGERNAUT, COVEN_RANDOM_NEUTRAL, COVEN_NEUTRAL_KILLING, COVEN_ANY }).FindAll(role => RoleList.Contains(Roles[(int)role])).Any();
+        private static bool Prefix() => new List<CustomGameSetup.Role>(new[] { WEREWOLF, RANDOM_NEUTRAL, NEUTRAL_KILLING, ANY, JUGGERNAUT, COVEN_RANDOM_NEUTRAL, COVEN_NEUTRAL_KILLING, COVEN_ANY }).FindAll(role => RoleList.Contains(Roles[(int)role])).Any();
     }
 }

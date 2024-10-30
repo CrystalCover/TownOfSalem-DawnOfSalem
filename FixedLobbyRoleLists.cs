fa7776b3de67d3d49ce712dfc6df2380351e4059
Eca <65168018+CrystalCover@users.noreply.github.com>
2024-10-30 23:51:17 +0300

    Update FixedLobbyRoleLists.cs

--- FixedLobbyRoleLists.cs
+++ FixedLobbyRoleLists.cs
@@ -1,9 +1,11 @@
 using HarmonyLib;
+using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace Eca.DawnOfSalem
 {
     [HarmonyPatch(typeof(BaseLobbySceneController))]
-    internal class FixedLobbyRoleList
+    internal class FixedLobbyRoleLists
     {
         private static ILobbyService LobbyService => GlobalServiceLocator.LobbyService;
 
@@ -11,15 +13,37 @@ namespace Eca.DawnOfSalem
 
         private static GameMode GameMode => CurrentLobby.GameMode;
 
+        private class OnRoleButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+        {
+            internal BaseLobbySceneController BaseLobbySceneController { get; set; }
+
+            private LobbyRoleDescriptionController LobbyRoleDescription => BaseLobbySceneController.LobbyRoleDescription;
+
+            internal Role Role { get; set; }
+
+            public void OnPointerEnter(PointerEventData eventData)
+            {
+                LobbyRoleDescription?.gameObject.SetActive(true);
+                LobbyRoleDescription?.SetRole(Role);
+            }
+
+            public void OnPointerExit(PointerEventData eventData) => LobbyRoleDescription?.gameObject.SetActive(false);
+        }
+
         [HarmonyPatch("AutoStartLobbyIfWeAreLastPlayer")]
         [HarmonyPrefix]
         private static void AutoStartLobbyIfWeAreLastPlayer(BaseLobbySceneController __instance)
         {
-            if (GameMode.LobbyType == LobbyType.Custom || GameMode.RoleList == null)
+            __instance.LobbyRoleDescription?.gameObject.SetActive(false);
+            if (GameMode.RoleList == null)
                 return;
             __instance.RoleList.Clear();
             foreach (Role role in GameMode.RoleList)
-                ((RoleListItem)__instance.RoleList.AddItem(role)).OnRoleButtonClicked += delegate { __instance.LobbyRoleDescription?.SetRole(role); };
+            {
+                OnRoleButtonHover onRoleButtonHover = __instance.RoleList.AddItem(role).gameObject.AddComponent<OnRoleButtonHover>();
+                onRoleButtonHover.BaseLobbySceneController = __instance;
+                onRoleButtonHover.Role = role;
+            }
             __instance.RoleCountLabel.SetText(__instance.RoleList.Count.ToString());
         }
 
@@ -32,6 +56,11 @@ namespace Eca.DawnOfSalem
             return false;
         }
 
+
+        [HarmonyPatch("HandleClickRoleSelected")]
+        [HarmonyPrefix]
+        private static void HandleClickRoleSelected(BaseLobbySceneController __instance) => __instance.LobbyRoleDescription?.gameObject.SetActive(true);
+
         [HarmonyPatch("HandleServerOnUserLeftLobby")]
         [HarmonyPrefix]
         private static bool HandleServerOnUserLeftLobby(BaseLobbySceneController __instance, LobbyUser user)
@@ -39,6 +68,15 @@ namespace Eca.DawnOfSalem
             __instance.UserList.RemoveListItem(user);
             __instance.PlayerCountLabel.SetText(__instance.UserList.Count.ToString());
             return false;
+        }
+
+        [HarmonyPatch("HandleOnRoleAdded")]
+        [HarmonyPostfix]
+        private static void HandleOnRoleAdded(BaseLobbySceneController __instance, Role role)
+        {
+            OnRoleButtonHover onRoleButtonHover = __instance.RoleList.GetListItem(__instance.RoleList.Count - 1).gameObject.AddComponent<OnRoleButtonHover>();
+            onRoleButtonHover.BaseLobbySceneController = __instance;
+            onRoleButtonHover.Role = role;
         }
     }
 }

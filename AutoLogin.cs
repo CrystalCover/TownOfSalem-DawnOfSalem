60555265a4a140903e496dec4bdc5ced712dafe4
Eca <65168018+CrystalCover@users.noreply.github.com>
2024-10-30 23:50:55 +0300

    Update AutoLogin.cs

--- AutoLogin.cs
+++ AutoLogin.cs
@@ -1,41 +1,54 @@
 using HarmonyLib;
-using System;
-using UnityEngine;
 using static Client.LoginType;
+using static ClientXMLFlags;
+using static ClientXMLFlags.ClientXmlFlag;
+using static Crypto;
 using static Scene;
+using static StringUtils;
+using static System.Convert;
+using static System.Guid;
+using static System.Reflection.BindingFlags;
+using static System.Text.Encoding;
+using static UnityEngine.PlayerPrefs;
 
 namespace Eca.DawnOfSalem
 {
-    [HarmonyPatch(typeof(ApplicationService))]
+    [HarmonyPatch(typeof(ApplicationService), nameof(ApplicationService.ShowScene))]
     internal class AutoLogin
     {
         private static bool AutoLoggedIn { get; set; } = false;
+
+        private static string UsernameKey => (string)typeof(BaseLoginSceneController).GetField("LOGIN_KEY", Static | Public | NonPublic).GetValue(null);
+
+        private static string PasswordKey => (string)typeof(BaseLoginSceneController).GetField("PASSWORD_KEY", Static | Public | NonPublic).GetValue(null);
 
         private static ISteamService SteamService => GlobalServiceLocator.SteamService;
 
         private static IUserService UserService => GlobalServiceLocator.UserService;
 
-        private static readonly byte[] keyBytes = Array.ConvertAll(new[] { 100, 49, 98, 51, 53, 48, 54, 48, 51, 55, 57, 56, 52, 100, 55, 48, 57, 98, 48, 100, 98, 57, 49, 48, 97, 56, 51, 97, 53, 48, 52, 49 }, @int => Convert.ToByte(@int));
+        private static byte[] KeyBytes => ASCII.GetBytes(HasKey(KeyBytesKey) ? SimpleEncryptDecrypt(GetString(KeyBytesKey), 653252328) : NewGuid().ToString("N"));
 
-        private static readonly byte[] ivBytes = Array.ConvertAll(new[] { 100, 50, 53, 100, 50, 48, 99, 57, 101, 50, 50, 51, 52, 101, 48, 48 }, @int => Convert.ToByte(@int));
+        private static string KeyBytesKey => (string)typeof(BaseLoginSceneController).GetField("CRYP_KEY", Static | Public | NonPublic).GetValue(null);
 
-        private static string Username => Crypto.AESDecrypt(Convert.FromBase64String(PlayerPrefs.GetString("Login.Username")), keyBytes, ivBytes);
+        private static byte[] IVBytes => ASCII.GetBytes(HasKey(IVBytesKey) ? SimpleEncryptDecrypt(GetString(IVBytesKey), 653252328) : NewGuid().ToString("N").Substring(0, 16));
 
-        private static string Password => Crypto.AESDecrypt(Convert.FromBase64String(PlayerPrefs.GetString("Login.Password")), keyBytes, ivBytes);
+        private static string IVBytesKey => (string)typeof(BaseLoginSceneController).GetField("IV_KEY", Static | Public | NonPublic).GetValue(null);
 
-        [HarmonyPatch("ShowScene")]
-        [HarmonyPrefix]
-        private static bool ShowScene(ApplicationService __instance, Scene scene)
+        private static string Username => AESDecrypt(FromBase64String(GetString(UsernameKey)), KeyBytes, IVBytes);
+
+        private static string Password => AESDecrypt(FromBase64String(GetString(PasswordKey)), KeyBytes, IVBytes);
+
+        private static bool Prefix(ApplicationService __instance, Scene scene)
         {
             if ((scene != Login && scene != BigLogin) || AutoLoggedIn)
                 return true;
             AutoLoggedIn = true;
-            if (!PlayerPrefs.HasKey("Login.Username") || !PlayerPrefs.HasKey("Login.Password"))
+            if (!HasKey(UsernameKey) || !HasKey(PasswordKey))
                 return true;
             if (SteamService.IsInitialized)
-                UserService.LoginViaSteamUsername(SteamService.GetUserSteamID(), Username, Password, SteamUsernamePassword, __instance.VersionInfo.build, ClientXMLFlags.GetClientXmlFlagSetting(ClientXMLFlags.ClientXmlFlag.AutoreportHostServerDisconnects));
+                UserService.LoginViaSteamUsername(SteamService.GetUserSteamID(), Username, Password, SteamUsernamePassword, __instance.VersionInfo.build, GetClientXmlFlagSetting(AutoreportHostServerDisconnects));
             else
-                UserService.Login(Username, Password, Web, __instance.VersionInfo.build, ClientXMLFlags.GetClientXmlFlagSetting(ClientXMLFlags.ClientXmlFlag.AutoreportHostServerDisconnects));
+                UserService.Login(Username, Password, Web, __instance.VersionInfo.build, GetClientXmlFlagSetting(AutoreportHostServerDisconnects));
             return false;
         }
     }
